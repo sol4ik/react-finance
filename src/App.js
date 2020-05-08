a73c91d9368d40f4768bd6dde737409066bcd45a
solka13Lenio <solomialenio13@gmail.com>
2020-05-08 15:01:51 +0300

    small refactor

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import FinancePage from './finance-page/finance-page';
 
+import './app.css';
+
 function App() {
   return (
     <div className="App">
